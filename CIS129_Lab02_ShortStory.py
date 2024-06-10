96106ea9bffd0f5de104e6f818d445e525ccfeab
yygaspar <ygaspar@mail.pima.edu>
2024-06-09 19:01:48 -0700

    Updated_Story_ CIS129_Lab02_ShortStory.py
    Entered and updated short story. Entered new commits including author name and code description

--- CIS129_Lab02_ShortStory.py
+++ CIS129_Lab02_ShortStory.py
@@ -1,6 +1,12 @@
-#Module 02 Lab - 06/09/2024
+#Yessenia Yvette Gaspar
+#First Python Lab during CIS129 course
+#Prints a simple short story
 
 
+print("Good day! It's short story time.")
+print("There was a little snake that wanted to create a video game.")
+print("Later one day, the snake entered a coding class to create one.")
+print("After years of learning, the snake created a game that was about capturing and eating eggs.")
+print("The snake sold the game for $401k and retired early.")
+print("The end.")
 
-print('Hello, World!")
-      
